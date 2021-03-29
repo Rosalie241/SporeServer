1f36ef7f0586115daf076cec834eee2bb0fdd1d4
Rosalie Wanders <rosalie@mailbox.org>
2021-03-29 16:25:55 +0200

    Add LICENSE

--- SporeMods/SporeNewOpenssl/dllmain.cpp
+++ SporeMods/SporeNewOpenssl/dllmain.cpp
@@ -1,3 +1,12 @@
+/*
+ * SporeServer - https://github.com/Rosalie241/SporeServer
+ *  Copyright (C) 2021 Rosalie Wanders <rosalie@mailbox.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3.
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 // dllmain.cpp : Defines the entry point for the DLL application.
 #define _CRT_SECURE_NO_WARNINGS
 // needed
