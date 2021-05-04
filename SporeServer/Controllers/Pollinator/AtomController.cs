df240c935b4bed00e3b7bfee219c6801ead95b43
Rosalie Wanders <rosalie@mailbox.org>
2021-05-04 15:46:26 +0200

    SporeServer: partially implement /pollinator/atom/downloadQueue

--- SporeServer/Controllers/Pollinator/AtomController.cs
+++ SporeServer/Controllers/Pollinator/AtomController.cs
@@ -70,53 +70,15 @@ namespace SporeServer.Controllers.Pollinator
 
         // GET /pollinator/atom/downloadQueue
         [HttpGet("downloadQueue")]
-        public IActionResult DownloadQueue()
+        public async Task<IActionResult> DownloadQueue()
         {
             Console.WriteLine($"/pollinator/atom/downloadQueue{Request.QueryString}");
 
+            var user = await _userManager.GetUserAsync(User);
 
-            string date = XmlConvert.ToString(DateTime.Now, XmlDateTimeSerializationMode.Utc);
-            /*
-            return new ContentResult()
-            {
-                Content = $@"<?xml version=""1.0"" encoding=""UTF-8""?>
-<feed xmlns=""http://www.w3.org/2005/Atom"" xmlns:sp=""http://spore.com/atom"" xml:lang=""en_US"">
-   <id>tag:spore.com,2006:downloadQueue</id>
-   <title>Rosalie</title>
-   <updated>{date}</updated>
-   <author>
-      <name>Rosalie</name>
-      <uri>13</uri>
-   </author>
-   <subcount>0</subcount>
-   <link rel=""self"" href=""https://pollinator.spore.com/pollinator/atom/downloadQueue"" />
-   <entry>
-      <id>tag:spore.com,2006:asset/501073471139</id>
-      <title>Scythovatid</title>
-      <link rel=""alternate"" href=""https://pollinator.spore.com/pollinator/sporepedia#qry=sast-501073471139"" />
-      <updated>{date}</updated>
-      <author>
-         <name>TheAWKLORD</name>
-         <uri>501071978458</uri>
-      </author>
-      <modeltype>0x9ea3031a</modeltype>
-      <locale>en_US</locale>
-      <modeltype>0x9ea3031a</modeltype>
-      <sp:ownership>
-         <sp:original name=""id"" type=""int"">501073471139</sp:original>
-         <sp:parent name=""id"" type=""int"">0</sp:parent>
-      </sp:ownership>
-      <content type=""html"">
-         <img src=""https://static.spore.com/tmp/501073471139.png3"" />
-      </content>
-      <link rel=""enclosure"" href=""https://static.spore.com/tmp/501073471139.png"" type=""image/png"" length=""80483"" />
-      <link rel=""enclosure"" href=""https://static.spore.com/tmp/501073471139.xml"" type=""application/x-creature+xml"" />
-      <summary>Following the discovery of rare underground spice geysers in 4780 C.S.E. was a series of reunification wars led by the Vaan'kthulvid dynasty in their efforts to monopolize this highly lucrative and powerful spice variant.</summary>
-   </entry>
-</feed>",
-                ContentType = "application/atom+xml"
-            };*/
-            return Ok();
+            return AtomFeedBuilder.CreateFromTemplate(
+                    new DownloadQueueTemplate(user, null)
+                ).ToContentResult();
         }
 
         // GET /pollinator/atom/asset/{id?}
