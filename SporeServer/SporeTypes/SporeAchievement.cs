9c46f45d94699fc5df59413b7ca0dd24d3a9a583
Rosalie Wanders <rosalie@mailbox.org>
2021-05-07 15:32:03 +0200

    SporeServer: add SporeAchievement to SporeTypes

---
+++ SporeServer/SporeTypes/SporeAchievement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SporeServer.SporeTypes
+{
+    public struct SporeAchievement
+    {
+        /// <summary>
+        ///     Id of achievement
+        /// </summary>
+        public Int64 Id { get; set; }
+        /// <summary>
+        ///     Name of Achievement
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        ///     Description of Achievement
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        ///     FileName when Achievement is unlocked
+        /// </summary>
+        public string UnlockedFileNameHash { get; set; }
+        /// <summary>
+        ///     FileName when Achievement is locked
+        /// </summary>
+        public string LockedFileNameHash { get; set; }
+        /// <summary>
+        ///     Whether the Achievement is secret or not
+        /// </summary>
+        public bool Secret { get; set; }
+    }
+}
