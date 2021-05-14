4eb6ed3e91890db19510bb768cee5544a66b4cc6
Rosalie Wanders <rosalie@mailbox.org>
2021-05-14 18:18:23 +0200

    SporeServer: add SporeEventType to SporeTypes

---
+++ SporeServer/SporeTypes/SporeEventType.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * SporeServer - https://github.com/Rosalie241/SporeServer
+ *  Copyright (C) 2021 Rosalie Wanders <rosalie@mailbox.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3.
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace SporeServer.SporeTypes
+{
+    public enum SporeEventType : Int64
+    {
+        AchievementUnlocked = 0x1edc82b0
+    };
+}
