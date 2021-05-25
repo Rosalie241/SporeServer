cdfa4be1fa68e623036105bbd11fccdc37cc99d9
Rosalie Wanders <rosalie@mailbox.org>
2021-05-25 13:23:46 +0200

    SporeServer: add missing license headers

--- SporeServer/Areas/Identity/Data/SporeServerAsset.cs
+++ SporeServer/Areas/Identity/Data/SporeServerAsset.cs
@@ -7,6 +7,15 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+/*
+ * SporeServer - https://github.com/Rosalie241/SporeServer
+ *  Copyright (C) 2021 Rosalie Wanders <rosalie@mailbox.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3.
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 using SporeServer.SporeTypes;
 using System;
 using System.Collections.Generic;
