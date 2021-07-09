974bd8dba1c7a17f7d51fa9e4ce46765763178d4
Rosalie Wanders <rosalie@mailbox.org>
2021-07-09 09:45:41 +0200

    SporeServer: partially implement /community/assetBrowser/home

--- SporeServer/Pages/Community/AssetBrowser/Home.cshtml.cs
+++ SporeServer/Pages/Community/AssetBrowser/Home.cshtml.cs
@@ -18,37 +18,48 @@ using SporeServer.Data;
 using Microsoft.AspNetCore.Identity;
 using SporeServer.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
+using SporeServer.Services;
 
 namespace SporeServer.Pages.Community.AssetBrowser
 {
     [Authorize]
     public class HomeModel : PageModel
     {
-        private readonly SporeServerContext _context;
         private readonly UserManager<SporeServerUser> _userManager;
+        private readonly IUserSubscriptionManager _userSubscriptionManager;
+        private readonly IAggregatorManager _aggregatorManager;
+        private readonly IAssetManager _assetManager;
 
-        public HomeModel(SporeServerContext context, UserManager<SporeServerUser> userManager)
+        public HomeModel(UserManager<SporeServerUser> userManager, IUserSubscriptionManager userSubscriptionManager, IAggregatorManager aggregatorManager, IAssetManager assetManager)
         {
-            _context = context;
             _userManager = userManager;
+            _userSubscriptionManager = userSubscriptionManager;
+            _aggregatorManager = aggregatorManager;
+            _assetManager = assetManager;
         }
 
-        public int GetSubscriberCountAsync()
+        /// <summary>
+        ///     Subscriber Count
+        /// </summary>
+        public Int32 SubscribeCount { get; set; }
+        /// <summary>
+        ///     Aggregator Count
+        /// </summary>
+        public Int32 AggregatorCount { get; set; }
+        /// <summary>
+        ///     Asset Count
+        /// </summary>
+        public Int32 AssetCount { get; set; }
+
+        public async Task<IActionResult> OnGet()
         {
-           // SporeServerUser user = await _userManager.GetUserAsync(User);
+            var user = await _userManager.GetUserAsync(User);
 
-            //if (user == null)
-            //{
-              //  return -1;
-            //}
+            SubscribeCount = await _userSubscriptionManager.GetCountByUserAsync(user);
+            AggregatorCount = await _aggregatorManager.GetCountByAuthorAsync(user);
+            AssetCount = await _assetManager.GetCountByAuthor(user);
 
-            return 0; // _context.Users.Where(u => u.Buddies.Contains(user)).Count();
-        }
-
-        public void OnGet()
-        {
-          
-
+            return Page();
         }
     }
 }
