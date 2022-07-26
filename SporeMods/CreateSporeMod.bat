de558aaba06aa900dd1f169b1bbbe9e5363da717
Rosalie Wanders <rosalie@mailbox.org>
2022-07-26 16:35:08 +0200

    SporeMods: fix CreateSporeMod.bat

--- SporeMods/CreateSporeMod.bat
+++ SporeMods/CreateSporeMod.bat
@@ -5,7 +5,7 @@ mkdir "%CUR_DIR%\obj"
 mkdir "%CUR_DIR%\bin"
 
 copy "%CUR_DIR%\SporeRedirectTraffic\Bin\Release\SporeRedirectTraffic.dll" "%CUR_DIR%\obj\"
-copy "%CUR_DIR%\SporeNewOpenSSL\Bin\Release\SporeNewOpenSSL.dll "%CUR_DIR%\obj\"
+copy "%CUR_DIR%\SporeNewOpenSSL\Bin\Release\SporeNewOpenSSL.dll" "%CUR_DIR%\obj\"
 copy "%CUR_DIR%\ModInfo.xml" "%CUR_DIR%\obj\"
 
 cd "%CUR_DIR%\obj\"
