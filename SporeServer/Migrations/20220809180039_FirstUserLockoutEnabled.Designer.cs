9254eb28f2d5307d575d6f38dcf8fc4007c0895f
Rosalie Wanders <rosalie@mailbox.org>
2022-08-09 20:02:45 +0200

    SporeServer: add FirstUserLockoutEnabled migration

--- SporeServer/Migrations/20220809180039_FirstUserLockoutEnabled.Designer.cs
+++ SporeServer/Migrations/20220809180039_FirstUserLockoutEnabled.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SporeServer.Data;
 
@@ -10,9 +11,10 @@ using SporeServer.Data;
 namespace SporeServer.Migrations
 {
     [DbContext(typeof(SporeServerContext))]
-    partial class SporeServerContextModelSnapshot : ModelSnapshot
+    [Migration("20220809180039_FirstUserLockoutEnabled")]
+    partial class FirstUserLockoutEnabled
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SporeServer/Migrations/20220809180039_FirstUserLockoutEnabled.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SporeServer.Migrations
+{
+    public partial class FirstUserLockoutEnabled : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "ConcurrencyStamp", "LockoutEnabled", "SecurityStamp" },
+                values: new object[] { "e90e461b-9070-43cd-bf04-0153367cabc5", true, "2afed3c2-1034-4c26-8b2d-4a15271cf76b" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "ConcurrencyStamp", "LockoutEnabled", "SecurityStamp" },
+                values: new object[] { "c536d83c-2c6c-448a-a82b-e1a58812c84a", false, "f11be4da-ad08-43dd-980a-cd8b6080ddcc" });
+        }
+    }
+}
