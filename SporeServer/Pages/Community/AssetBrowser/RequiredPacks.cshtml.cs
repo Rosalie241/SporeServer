ae6d6b6bc14f18363e8bc991b66ebdd46937b4a4
Rosalie Wanders <rosalie@mailbox.org>
2022-08-10 13:09:44 +0200

    SporeServer: add [Authorize] attribute to /community/assetBrowser/requiredPacks

--- SporeServer/Pages/Community/AssetBrowser/RequiredPacks.cshtml.cs
+++ SporeServer/Pages/Community/AssetBrowser/RequiredPacks.cshtml.cs
@@ -1,12 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+/*
+ * SporeServer - https://github.com/Rosalie241/SporeServer
+ *  Copyright (C) 2021 Rosalie Wanders <rosalie@mailbox.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3.
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SporeServer.Pages.Community.AssetBrowser
 {
+    [Authorize]
     public class RequiredPacksModel : PageModel
     {
         public void OnGet()
