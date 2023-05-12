f1bf7d1fdae77487656bf168b6bf8b3f70a55cc9
Rosalie Wanders <rosalie@mailbox.org>
2023-05-12 19:05:07 +0200

    SporeServer: add SporeMouthType and SporeArcheTypes to SporeTypes

---
+++ SporeServer/SporeTypes/SporeMouthType.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * SporeServer - https://github.com/Rosalie241/SporeServer
+ *  Copyright (C) 2021 Rosalie Wanders <rosalie@mailbox.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3.
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+namespace SporeServer.SporeTypes
+{
+    public enum SporeMouthType
+    {
+        Carnivore = 0,
+        Herbivore = 1,
+        Omnivore = 2,
+        Any      = 3
+    };
+}
